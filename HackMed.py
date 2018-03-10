c8fed5f3e07eb13414bf20b545b09a983bdfbbab
Naomi <limaronathan@gmail.com>
2018-03-10 17:03:52 +0000

    form

--- HackMed.py
+++ HackMed.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -13,6 +13,15 @@ def hackmed():
     replace_me = "HERE I AM"
 
     return render_template('hackmed.html',replace_me=replace_me)
+
+@app.route('/process_data',methods=["POST"])
+def process_data():
+    name = request.form["name"]
+
+    #nexmo python message api
+
+    return render_template('hackmed_response.html',name=name)
+
 
 if __name__ == '__main__':
     app.run()
